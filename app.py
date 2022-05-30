fea8cac1341afb5bc6e560f81e3a408d32d0f027
Mohammad Eskandari <mohesk@users.noreply.github.com>
2022-05-30 12:03:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,11 @@ def callback_post_request():
 def home():
     return "EENVEST CALLBACK POST REQUEST"
 
-
-app.run()
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
