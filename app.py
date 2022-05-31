dacee9f6f1ddf024eb488cba7538b68efc2cd048
Mohammad Eskandari <mohesk@users.noreply.github.com>
2022-05-31 09:40:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,52 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 import json
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 
-@app.route('/callback', methods=['POST'])
-def callback_post_request():
-    print(json.dumps(request.json))
-    return json.dumps(request.json), 200
+@app.route('/jobstatus/<jobuuid>', methods=['POST'])
+def callback(jobuuid):
+    print({"msg": f"job id {jobuuid} is done!"})
+    f = open("static/status.txt", "a")
+    f.write(f"{jobuuid}\n\r")
+    f.close()
+    return {"msg": f"job id {jobuuid} is done!"}
+
+
+@app.route('/jobCreate/<jobuuid>', methods=['POST'])
+def callbackCreate(jobuuid):
+    print({"msg": f"job id {jobuuid} is created!"})
+    f = open("static/job.txt", "a")
+    f.write(f"{jobuuid}\n\r")
+    f.close()
+    return {"msg": f"job id {jobuuid} is created!"}
 
 
 @app.route('/')
 def home():
-    return "EENVEST CALLBACK POST REQUEST"
+    return render_template('index.html')
+
+
+@app.route('/callback', methods=['POST'])
+def callback_post_request():
+    f = open("static/body.txt", "w")
+    f.write(json.dumps(request.json))
+    f.close()
+    print(json.dumps(request.json))
+    return json.dumps(request.json)
+
+
+@app.route('/reset', methods=['GET'])
+def reset_get_request():
+    f = open("static/body.txt", "w")
+    f.write(json.dumps("{}"))
+    f.close()
+    print(json.dumps(request.json))
+    return json.dumps(request.json)
+
 
 if __name__ == '__main__':
-    import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
